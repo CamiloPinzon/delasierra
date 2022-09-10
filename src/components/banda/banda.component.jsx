d8d24f3d0d776a793deba27dc0e78398425d28e6
Camilo Pinzon <acamilopinzon-ext@alyce.com>
2022-09-10 08:28:28 -0500

    add banda component

---
+++ src/components/banda/banda.component.jsx
@@ -0,0 +1,14 @@
+import './banda.styles.scss';
+
+const Banda = () => {
+    return (
+        <div className="banda-container">
+            <h2>DELASIERRA BANDA</h2>
+            <p>Somos una banda independiente, con varios años de trabajo y trayectoria en la capital colombiana. Sonamos en diferentes latitudes y géneros musicales, en las que sobresalen el Reggae, el Rocksteady y la Kizomba.</p>
+            <p>Años de experiencia en otros proyectos musicales nos consolidan hoy como una agrupación responsable, comprometida, sólida y muy trabajadora. Buscamos llevar nuestras canciones a niveles superiores de audiencia y visibilidad, soñamos con los festivales más importantes del país y del planeta.</p>
+            <p>Con nuestro seudónimo -DELASIERRA- cargamos la responsabilidad y la identidad de un territorio que nos pertenece, nos llena de música y de conocimiento (La Sierra Nevada de Santa Marta).</p>
+        </div>
+    )
+}
+
+export default Banda;
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Hero from "../../components/hero/hero.component";
+import Banda from "../../components/banda/banda.component";
 
 const Home = () => {
     return(
         <div className="home-container">
             <Hero />
+            <Banda />
         </div>
     )
 }
